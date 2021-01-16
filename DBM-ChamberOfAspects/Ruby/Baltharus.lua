cc72c305384446b97cd91fd2f0710c5edbe99299
Maevis <apc.xfiles@gmail.com>
2021-01-16 21:34:32 +0800

    Add files via upload

--- DBM-ChamberOfAspects/Ruby/Baltharus.lua
+++ DBM-ChamberOfAspects/Ruby/Baltharus.lua
@@ -16,10 +16,14 @@ mod:RegisterEvents(
 local warningSplitSoon		= mod:NewAnnounce("WarningSplitSoon", 2)
 local warningRepellingWave	= mod:NewSpellAnnounce(74509, 3)
 local warnWhirlwind			= mod:NewSpellAnnounce(75125, 3, nil, mod:IsTank() or mod:IsHealer())
+local warningWarnBrand		= mod:NewTargetAnnounce(74505, 4)
+
+local specWarnBrand			= mod:NewSpecialWarningYou(74505)
 local specWarnRepellingWave	= mod:NewSpecialWarningSpell(74509)
 
 local timerWhirlwind		= mod:NewBuffActiveTimer(4, 75125, nil, mod:IsTank() or mod:IsHealer())
 local timerRepellingWave	= mod:NewBuffActiveTimer(4, 74509)--1 second cast + 3 second stun
+local timerBrand			= mod:NewBuffActiveTimer(10, 74505)
 
 mod:AddBoolOption("SetIconOnBrand", true)
 mod:AddBoolOption("RangeFrame")
@@ -27,6 +31,13 @@ mod:AddBoolOption("RangeFrame")
 local warnedSplit1	= false
 local warnedSplit2	= false
 local warnedSplit3	= false
+local brandTargets = {}
+local brandIcon	= 8
+
+local function showBrandWarning()
+	warningWarnBrand:Show(table.concat(brandTargets, "<, >"))
+	table.wipe(brandTargets)
+end
 
 function mod:OnCombatStart(delay)
 	warnedSplit1 = false
@@ -51,12 +62,29 @@ function mod:SPELL_CAST_START(args)
 		specWarnRepellingWave:Show()
 		timerRepellingWave:Show()
 	end
+end
 
-local function mod:SPELL_AURA_APPLIED(args)
+function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(75125) then
 		warnWhirlwind:Show()
 		timerWhirlwind:Show()
+	elseif args:IsSpellID(74505) then
+		brandTargets[#brandTargets + 1] = args.destName
+		if args:IsPlayer() then
+			specWarnBrand:Show()
+			timerBrand:Show()
 		end
+		if self.Options.SetIconOnBrand then
+			if 	brandIcon < 1 then
+				brandIcon = 8
+			end
+			self:SetIcon(args.destName, brandIcon, 10)
+			brandIcon = brandIcon - 1
+		end
+		self:Unschedule(showBrandWarning)
+		self:Schedule(0.5, showBrandWarning)
+	end
+end
 
 function mod:UNIT_HEALTH(uId)
 	if (mod:IsDifficulty("normal25") or mod:IsDifficulty("heroic25")) then
